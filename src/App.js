5ab9e81e0dd9d34e565a0f6d7285e88b6873abe8
HuyNgocDuong <ngochuyduong288@gmail.com>
2025-02-24 01:13:35 +1100

    debug,  add the marker

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
 import Map from './components/Map'
+import { useState, useEffect } from 'react'
 
 function App() {
+  const [eventData, setEventData] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      setLoading(true);
+      try {
+        const res = await fetch('https://eonet.sci.gsfc.nasa.gov/api/v2.1/events');
+        if (!res.ok) throw new Error(`HTTP error! Status: ${res.status}`);
+        
+        const { events } = await res.json();
+        setEventData(events);
+      } catch (error) {
+        console.error("Error fetching events:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchEvents();
+  }, []);
+
+  useEffect(() => {
+    console.log("Updated eventData:", eventData);
+  }, [eventData]);
+
   return (
     <div>
-        <Map />
+      {loading ? <h2>Loading...</h2> : <Map eventData={eventData} />}
     </div>
   );
 }
